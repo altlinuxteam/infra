970efb4b997b9753af1c648b4266956ee1401d14
Evgeny Sinelnikov <sin@altlinux.org>
2021-09-18 07:02:21 +0400

    proxmoxer: update with fixes from next release

--- nix/proxmoxer/default.nix
+++ nix/proxmoxer/default.nix
@@ -12,10 +12,12 @@ python27.pkgs.buildPythonPackage rec {
   };
 
   patches = [ ./show_reason_on_exceptions.patch ];
+  patches = [ ./added_timeout_settings_for_auth_request.diff ];
+  patches = [ ./fix_pvesh_output_format_for_version_more_than_5.3.diff ];
   doCheck = false;
 
   meta = {
-    homepage = "https://github.com/swayf/proxmoxer";
+    homepage = "https://github.com/proxmoxer/proxmoxer";
     description = "Proxmoxer is a wrapper around the Proxmox REST API v2";
   };
 }
