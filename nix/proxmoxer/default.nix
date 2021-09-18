390cfa18d31415948e890891dc27174adefedd4c
Evgeny Sinelnikov <sin@altlinux.org>
2021-09-18 07:09:15 +0400

    proxmoxer: replace patches in single patch

--- nix/proxmoxer/default.nix
+++ nix/proxmoxer/default.nix
@@ -11,9 +11,7 @@ python27.pkgs.buildPythonPackage rec {
     sha256 = "145hvphvlzvwq6sn31ldnin0ii50blsapxz0gv2zx3grzp6x9hvh";
   };
 
-  patches = [ ./show_reason_on_exceptions.patch ];
-  patches = [ ./added_timeout_settings_for_auth_request.diff ];
-  patches = [ ./fix_pvesh_output_format_for_version_more_than_5.3.diff ];
+  patches = [ ./proxmoxer.patch ];
   doCheck = false;
 
   meta = {
